adc8b34176233e4047fd96bd57431992365a83f6
moyu979 <moyu979@gmail.com>
2024-05-08 19:12:33 +0800

    to be test

--- main.c
+++ main.c
@@ -1,16 +1,15 @@
 #include<REG51.h>
 
 #define FUNMASK 0xF0
+#define PINMASK 0x0F
 #define GROUPMASK 0x08
-#define PINMASK 0x07 
 
 unsigned char FIRST;
 unsigned char SECOND;
 
 unsigned char RET;
 
-sbit sP1[] = {P1^0,P1^1,P1^2,P1^3,P1^4,P1^5,P1^6,P1^7};
-sbit sP2[] = {P2^0,P2^1,P2^2,P2^3,P2^4,P2^5,P2^6,P2^7};
+sbit sP[][] = {P1^0,P1^1,P1^2,P1^3,P1^4,P1^5,P1^6,P1^7,P2^0,P2^1,P2^2,P2^3,P2^4,P2^5,P2^6,P2^7};
 
 void UART_Read_init(){
     EA=1;//总中断开
@@ -33,37 +32,68 @@ void UART_Read(void) interrupt 4 using 1{
     RI=0;
     SECOND=SBUF;
     
+    RET=0x00;
     switch (FIRST & FUNMASK)
     {
+    //查看某个GPIO引脚的值
     case 0x10:
-        getGpio();
+        RET=sP[FIRST & PINMASK]?0x01:0x00;
         break;
+    //查看某组GPIO的值
     case 0x20:
-        getGpioGroup();
+        if(FIRST & GROUPMASK){
+            RET=P2;
+        }else{
+            RET=P1;
+        }
         break;
+    //写入某个GPIO
     case 0x40:
-        writeGpio();
+        sp[FIRST & PINMASK]=SECOND?1:0;
         break;
+    //将某个GPIO与数据进行与操作
     case 0x50:
-        andGpio();
+        sp[FIRST & PINMASK]=(sp[FIRST & PINMASK])&(SECOND?1:0);
         break;
+    //将某个GPIO与数据进行或操作
     case 0x60:
-        orGpio();
+        sp[FIRST & PINMASK]=(sp[FIRST & PINMASK])|(SECOND?1:0);
         break;
+    //将某个GPIO与数据进行异或操作
     case 0x70:
-        xorGpio();
+        sp[FIRST & PINMASK]=(sp[FIRST & PINMASK])^(SECOND?1:0);
         break;
+    //写入某组GPIO
     case 0x80:
-        writeGpioGroup();
+        if(FIRST & GROUPMASK){
+            P2=SECOND;
+        }else{
+            P1=SECOND;
+        }
         break;
+    //将某组GPIO与数据进行与操作
     case 0x90:
-        andGpioGroup();
+        if(FIRST & GROUPMASK){
+            P2=P2&SECOND;
+        }else{
+            P1=P1&SECOND;
+        }
         break;
+    //将某组GPIO与数据进行或操作
     case 0xa0:
-        orGpioGroup();
+        if(FIRST & GROUPMASK){
+            P2=P2|SECOND;
+        }else{
+            P1=P1|SECOND;
+        }
         break;
+    //将某组GPIO与数据进行异或操作
     case 0xb0:
-        xorGpioGroup();
+        if(FIRST & GROUPMASK){
+            P2=P2^SECOND;
+        }else{
+            P1=P1^SECOND;
+        }
         break;
     default:
         break;
