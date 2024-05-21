1145be05224f5a1c28a5d485fb40ff8d77261a31
moyu979 <moyu979@gmail.com>
2024-05-21 19:11:55 +0800

    fix errors

--- main.c
+++ main.c
@@ -9,7 +9,7 @@ unsigned char FIRST;
 unsigned char SECOND;
 
 unsigned char RET;
-
+//{10,0,11,0,12,0,41,0,10,0,11,0,12,0}
 int start;
 
 unsigned char position[8]={0x01,0x02,0x04,0x08,0x10,0x20,0x40,0x80};
@@ -42,9 +42,9 @@ void UART_Read(void) interrupt 4 using 1{
                 //查看某个GPIO引脚的值
                 case 0x10:
                     if(FIRST&GROUPMASK){
-                        RET=P2^(FIRST&PINNUMBER)==1?'h':'l';
+                        RET=(P2&position[FIRST&PINNUMBER])==0?'l':'h';
                     }else{
-                        RET=P1^(FIRST&PINNUMBER)==1?'h':'l';
+                        RET=(P1&position[FIRST&PINNUMBER])==0?'l':'h';
                     }
                     break;
                 //查看某组GPIO的值
@@ -55,20 +55,13 @@ void UART_Read(void) interrupt 4 using 1{
                         RET=P1;
                     }
                     break;
-                //写入某组GPIO
-                case 0x80:
-                    if(FIRST & GROUPMASK){
-                        P2=SECOND;
-                    }else{
-                        P1=SECOND;
-                    }
-                    break;
+                
                 //写入某个GPIO
                 case 0x40:
                     if(FIRST&GROUPMASK){
                         //置0
 						if(SECOND==0){
-                            P2=P2&(!position[FIRST&PINNUMBER]);
+                            P2=P2&(~position[FIRST&PINNUMBER]);
                         }
                         //置1
                         else{
@@ -77,7 +70,7 @@ void UART_Read(void) interrupt 4 using 1{
                     }else{
                         //置0
 						if(SECOND==0){
-                            P1=P1&(!position[FIRST&PINNUMBER]);
+                            P1=P1&(~position[FIRST&PINNUMBER]);
                         }
                         //置1
                         else{
@@ -90,7 +83,7 @@ void UART_Read(void) interrupt 4 using 1{
                     if(FIRST&GROUPMASK){
                         //与0
                         if(SECOND==0){
-                            P2=P2&(!position[FIRST&PINNUMBER]);
+                            P2=P2&(~position[FIRST&PINNUMBER]);
                         }
                         //与1
                         else{
@@ -99,7 +92,7 @@ void UART_Read(void) interrupt 4 using 1{
                     }else{
                         //与0
                         if(SECOND==0){
-                            P1=P1&(!position[FIRST&PINNUMBER]);
+                            P1=P1&(~position[FIRST&PINNUMBER]);
                         }
                         //与1
                         else{
@@ -150,6 +143,14 @@ void UART_Read(void) interrupt 4 using 1{
                         }
                     }
                     break;
+                //写入某组GPIO
+                case 0x80:
+                    if(FIRST & GROUPMASK){
+                        P2=SECOND;
+                    }else{
+                        P1=SECOND;
+                    }
+                    break;
                 //将某组GPIO与数据进行与操作
                 case 0x90:
                     if(FIRST & GROUPMASK){
@@ -177,15 +178,13 @@ void UART_Read(void) interrupt 4 using 1{
                 default:
                     break;
             }
-                SBUF=RET;
-                TI=0;
+            P0=RET;
+            SBUF=RET;
+            TI=0;
         }
     }
 }
 
-    //switch (FIRST & FUNMASK)
-    //{
-    //
     
     
     
